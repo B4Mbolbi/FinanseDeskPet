98ee8a65f6d5ed0213a9d5e2564d76c56aea32fe
B4MBOLBI <tytor5674onli@gmail.com>
2024-11-03 15:56:06 +1000

    Обновление: Добавлен блок "Ваши финансы"

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
+  <div class="General flex-row">
+    <Desk></Desk>
 
-
+  </div>
 </template>
 
-<script setup>
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  transition: .5s;
+  display: flex;
+}
+.General {
+  width: 100%;
+  height: 100vh;
+  background-image: url("./assets/fone.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 
+}
+</style>
+
+
+<script>
+import Desk from '@/components/Desk/Desk.vue'
+export default {
+  components: {Desk}
+
+}
 </script>
-

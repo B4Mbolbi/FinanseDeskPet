ca7af78e6c5e850b49af24f9483ff5bb0192638e
B4MBOLBI <tytor5674onli@gmail.com>
2024-11-03 15:57:05 +1000

    Обновление: Добавлен блок "Ваши финансы"

---
+++ src/components/Desk/Desk.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-[50%] rounded-2xl h-[100%] flex-col text-white bg-black bg-opacity-[.6]">
+<!--      Header -->
+    <div class="w-full h-14 text-xl  bg-black font-bold bg-opacity-[.5] flex flex-row relative justify-start pl-2 items-center">Ваши финансы</div>
+
+      <div class="w-full h-[90%] bg-opacity-[.2] bg-black flex flex-row p-2 gap-3.5">
+
+        <div class="w-[50%] h-full">
+          <div class=" w-full h-[40px] flex justify-between items-center font-bold text-[12px]">
+            Доски
+            <button class="w-7 h-7 bg-amber-500 rounded-full"><img src="@/assets/plus.ico"></button>
+          </div>
+          <div class="w-full h-[90%] flex flex-col ">
+
+          </div>
+        </div>
+
+        <div class="w-[50%] h-full">
+          <div class=" w-full h-[40px] flex justify-between items-center font-bold text-[12px]">
+            Карточки
+            <button class="w-7 h-7 bg-amber-500 rounded-full"><img src="@/assets/plus.ico"></button>
+          </div>
+          <div class="w-full h-[90%]  flex flex-col gap-3.5 p-3">
+
+          </div>
+        </div>
+
+      </div>
+
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
